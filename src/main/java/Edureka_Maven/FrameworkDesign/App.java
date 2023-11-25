038ef583fd5288bb11c9834cb4923fbcade4150e
Ajith <ajithkumar.sb3298@gmail.com>
2023-11-25 18:06:20 +0530

    First Commit

---
+++ src/main/java/Edureka_Maven/FrameworkDesign/App.java
@@ -0,0 +1,13 @@
+package Edureka_Maven.FrameworkDesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
